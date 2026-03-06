64405eb4aa65fb6ab067f5f3908bb6cae866cda5
George Black <contact@george.black>
2026-03-06 17:56:10 -0600

    Init new worker for the mailman project

---
+++ mailman/src/index.ts
@@ -0,0 +1,16 @@
+export default {
+  async fetch(req) {
+    const url = new URL(req.url);
+    url.pathname = "/__scheduled";
+    url.searchParams.append("cron", "* * * * *");
+    return new Response(
+      `To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`,
+    );
+  },
+
+  async scheduled(event, env, ctx): Promise<void> {
+    let resp = await fetch("https://api.cloudflare.com/client/v4/ips");
+    let wasSuccessful = resp.ok ? "success" : "fail";
+    console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+  },
+} satisfies ExportedHandler<Env>;
