555d326d1ef2c3692ce78858c7a6c910cf5ec2fe
George Black <contact@george.black>
2026-03-06 17:49:47 -0600

    Migrate CMS workers to web repo

--- cms/src/components/PaddedSurface.tsx
+++
@@ -1,7 +0,0 @@
-import { Surface } from "@cloudflare/kumo";
-
-export function PaddedSurface(props: { children: React.ReactNode }) {
-  return <Surface className="p-4 rounded-lg">{props.children}</Surface>;
-}
-
-export default PaddedSurface;
